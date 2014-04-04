fdf3717b10fd5ee0a38e76abf430b66c0b9a3854
ceaparedes <vicea@alumnos.ubiobio.cl>
2014-04-04 10:54:17 -0400

    linea web modificada
    wena xoroo

--- TallerGit/src/cl/taller/git/main.java
+++ TallerGit/src/cl/taller/git/main.java
@@ -6,6 +6,6 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo xoroo");
 		System.out.println("wena xoro 2");
-		System.out.println("linea web");
+		System.out.println("linea web 69");
 	}
 }
